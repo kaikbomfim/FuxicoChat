a5d8a5258bbef9c030d64bca42947e20c972e8e2
Kaik Bomfim <kaikbomfim@gmail.com>
2024-03-27 22:41:45 -0300

    FuxicoChat

---
+++ src/main/java/com/fuxicochat/FuxicochatApplication.java
@@ -0,0 +1,13 @@
+package com.fuxicochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuxicochatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuxicochatApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fuxicochat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.fuxicochat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
